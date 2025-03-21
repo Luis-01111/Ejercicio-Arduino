3f615b0591d1e834970d77139a0954643d0acc3d
Luis-01111 <silvapenaluisfernando6@gmail.com>
2025-03-21 10:19:58 -0500

    Add files via upload

---
+++ bluetooth_potenciometro.ino
@@ -0,0 +1,79 @@
+#include <BluetoothSerial.h>
+
+#define pin 2
+
+#define pA 34
+
+String nombretarjeta = "Dafne y Luis 19", lecaux= " ";
+
+int cont = 0; 
+
+const float vmax =100 , vmin=0 , posmax=4095 , posmin=0; 
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT; 
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin (9600); 
+SerialBT.begin (nombretarjeta);   
+pinMode (pin, OUTPUT);
+
+}
+
+void loop() {
+  int lecPot = analogRead (pA);
+  Serial.println (lecPot);
+  Serial.println (calcularValor (lecPot));
+  delay (3000);  
+ 
+  String lectura = ""; 
+  if (SerialBT.available ()){
+    lectura = leerBT();
+    lectura.trim(); 
+    Serial.println (lectura); 
+  }
+
+  else {
+  }
+
+  if (lectura == "encendido"){
+    digitalWrite (pin,1); 
+  }
+
+  else if (lectura == "apagado"){
+    digitalWrite (pin,0); 
+  }
+
+  else {
+    Serial.println ("Valor no valido"); 
+  }
+  
+}
+
+
+String leerBT () {
+  lecaux = " "; 
+ char a = SerialBT.read();
+ lecaux = lecaux + a; 
+  while (SerialBT. available ()){
+    a = SerialBT.read();
+ lecaux = lecaux + a;     
+  }
+return lecaux; 
+}
+
+float calcularValor (int x) {
+float m = (vmax-vmin) / (posmax-posmin);
+float b = vmax -m*posmax; 
+float y = m*x+b; 
+return y;  
+}
